03ea9ee1f51521c832d908ead74c6dad06392c6c
Venessa <gomesvenessa@gmail.com>
2022-11-10 14:10:25 -0500

    css fixed

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(cookieParser());
 app.use(session({
   secret: 'SEIRocks!',
